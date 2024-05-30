ccddf65a87ec9e7a8d56bf29eaf070ab53dc3157
MonaLolisa <53138805+MonaLolisa@users.noreply.github.com>
2024-05-30 15:11:09 +0200

    Update JaLo test.js

--- JaLo test.js
+++ JaLo test.js
@@ -1,3 +1,8 @@
+/* apply the acrylic effect */
+window.Effect.apply('unified', { color: "#000000DA" });
+
+
+/* Import only theme */
 // index.js
 function addCssLink(url) {
   const link = document.createElement('link')
@@ -10,3 +15,21 @@ function addCssLink(url) {
 window.addEventListener('load', () => {
   addCssLink('https://raw.githubusercontent.com/MonaLolisa/JaLoLeagueSkin/main/cleantheme.css')
 })
+
+/* Import shadow-dom obeserver-fixer-thing */
+import * as observer from './observer'
+import * as shadow_dom from './shadow-dom'
+
+
+/* Code to edit the lobby info panel, leftmost panel, top of friend list */
+observer.subscribeToElementCreation('lol-parties-game-info-panel', (element) => {
+    shadow_dom.lobby_panel_css(element)
+
+})
+
+/* Code to edit the lobby invite notification, leftmost panel, top of friend list */
+observer.subscribeToElementCreation('lol-parties-game-invite', (element) => {
+    shadow_dom.lobby_invite_css(element)
+
+})
+
